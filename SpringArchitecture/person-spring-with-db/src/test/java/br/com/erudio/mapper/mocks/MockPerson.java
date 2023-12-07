440b52eb8341822fb0d35629fb4b394d75ade015
gregorio <igregoriodoneto@outlook.com>
2023-12-07 17:46:40 -0300

    feat: Testes com Mockito e JUnit do Service

---
+++ SpringArchitecture/person-spring-with-db/src/test/java/br/com/erudio/mapper/mocks/MockPerson.java
@@ -0,0 +1,50 @@
+package br.com.erudio.mapper.mocks;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockPerson {
+
+    public Person mockEntity() { return mockEntity(0); }
+
+    public PersonVO mockVO() { return mockVO(0); }
+
+    public List<Person> mockEntityList() {
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonVO> mockVOList() {
+        List<PersonVO> personsVO = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            personsVO.add(mockVO(i));
+        }
+        return personsVO;
+    }
+
+    public Person mockEntity(Integer number) {
+        Person person = new Person();
+        person.setAddress("Addres Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+    public PersonVO mockVO(Integer number) {
+        PersonVO person = new PersonVO();
+        person.setAddress("Addres Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setKey(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+}
